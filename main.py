ca11416ec07c2a438b5e9d2699db6b94a2c1697c
znnuser <2503488719@qq.com>
2025-06-18 09:09:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -72,6 +72,7 @@ class SeatAutoBooker:
             end_time = datetime.now().replace(hour=21-time_zone, minute=15, second=0, microsecond=0)
         start_time = start_time - timedelta(minutes=self.cfg["cron-delta-minutes"])
         if datetime.now() < start_time or datetime.now() > end_time:
+            logging.info('Login in')
             return -1, "未到预约时间"
         logging.info('Booking favorite seat')
         retry_sleep_time = timedelta(minutes=self.cfg["cron-delta-minutes"]).seconds*2/(self.cfg["max-retry"]-2) - 10
