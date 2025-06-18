88ae2a8f98327b972e8d67f00f494601251cb615
znnuser <2503488719@qq.com>
2025-06-18 09:00:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -65,27 +65,23 @@ class SeatAutoBooker:
         the_day_after_tomorrow = ['周一', '周二', '周三', '周四', '周五', '周六', '周日'][(datetime.now().weekday() + 2) % 7]
         seat_type = seat_config[user_config[the_day_after_tomorrow]['name']]["type"]
         if seat_type == "自习室":
-            start_time = datetime.now().replace(hour=7-time_zone, minute=0, second=0, microsecond=0)
-            end_time = datetime.now().replace(hour=9-time_zone, minute=15, second=0, microsecond=0)
-        else:
             start_time = datetime.now().replace(hour=8-time_zone, minute=0, second=0, microsecond=0)
             end_time = datetime.now().replace(hour=9-time_zone, minute=15, second=0, microsecond=0)
+        else:
+            start_time = datetime.now().replace(hour=21-time_zone, minute=0, second=0, microsecond=0)
+            end_time = datetime.now().replace(hour=21-time_zone, minute=15, second=0, microsecond=0)
         start_time = start_time - timedelta(minutes=self.cfg["cron-delta-minutes"])
         if datetime.now() < start_time or datetime.now() > end_time:
-            logging.info('卡着了')
             return -1, "未到预约时间"
         logging.info('Booking favorite seat')
         retry_sleep_time = timedelta(minutes=self.cfg["cron-delta-minutes"]).seconds*2/(self.cfg["max-retry"]-2) - 10
         for tried_times in range(self.cfg["max-retry"]):
             try:
-                print("Server酱")
                 return self._book_favorite_seat(user_config, seat_config, tried_times)
             except Exception as e:
                 logging.exception(e)
                 print(e.__class__, "尝试第{}次".format(tried_times))
                 time.sleep(retry_sleep_time)
-
-
 
     def _book_favorite_seat(self, user_config, seat_config, tried_times=0):
         logging.info('Entering _book_favorite_seat method')
